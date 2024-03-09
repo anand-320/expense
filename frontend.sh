5d77a47591458790371e86a352c857fde84d019b
anand <anandanand99599@gmail.com>
2024-03-09 11:36:21 +0530

    Adding redirectors

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,9 @@
-dnf install nginx -y >/tep/expense.log
-systemctl enable nginx >/tep/expense.log
-systemctl start nginx >/tep/expense.log
-cp expense.conf /etc/nginx/default.d/expense.conf >/tep/expense.log
-rm -rf /usr/share/nginx/html/* >/tep/expense.log
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >/tep/expense.log
-cd /usr/share/nginx/html >/tep/expense.log
-unzip /tmp/frontend.zip >/tep/expense.log
-systemctl restart nginx >/tep/expense.log
+dnf install nginx -y >/tmp/expense.log
+systemctl enable nginx >/tmp/expense.log
+systemctl start nginx >/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
+rm -rf /usr/share/nginx/html/* >/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >/tmp/expense.log
+cd /usr/share/nginx/html >/tmp/expense.log
+unzip /tmp/frontend.zip >/tmp/expense.log
+systemctl restart nginx >/tmp/expense.log
