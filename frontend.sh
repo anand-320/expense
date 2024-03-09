f3fe77ca2f48aff1869c630b89f98fb02368d1f5
anand <anandanand99599@gmail.com>
2024-03-09 11:50:40 +0530

    Adding echo command

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,18 @@
+echo install nginx
 dnf install nginx -y &>>/tmp/expense.log
+echo enable nginx
 systemctl enable nginx &>>/tmp/expense.log
+echo start nginx
 systemctl start nginx &>>/tmp/expense.log
+echo copy ecpense.conf
 cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+echo remove
 rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+echo download content
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
+echo change directory
 cd /usr/share/nginx/html &>>/tmp/expense.log
+echo unzip file
 unzip /tmp/frontend.zip &>>/tmp/expense.log
+echo restart nginx
 systemctl restart nginx &>>/tmp/expense.log
