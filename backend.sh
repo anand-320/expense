c1fadcc00351f5bfade90a98b3534f6c13f3d82d
anand <anandanand99599@gmail.com>
2024-03-09 13:09:32 +0530

    printing headings using functions

--- backend.sh
+++ backend.sh
@@ -1,20 +1,71 @@
+source common.sh
+
 mysql_root_password=$1
+
+print_heading "disable nodejs -y"
 dnf module disable nodejs -y
+echo $?
+
+print_heading "enable nodejs:20"
 dnf module enable nodejs:20 -y
+echo $?
+
+print_heading "install nodejs"
 dnf install nodejs -y
+echo $?
+
+print_heading "add user expense"
 useradd expense
+echo $?
+
+print_heading "copy backend esrvice file"
 cp backend.service /etc/systemd/system/backend.service
+echo $?
+
+print_heading "clear /appx"
 rm -rf /app
+echo $?
 
+print_heading "creating /app"
 mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
-unzip /tmp/backend.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
-dnf install mysql -y
-mysql -h 172.31.9.13 -uroot -p{mysql_root_password} < /app/schema/backend.sql
+echo $?
 
+print_heading "downloading content"
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+echo $?
+
+print_heading "moving to /app directory"
+cd /app
+echo $?
+
+print_heading "unzip the content"
+unzip /tmp/backend.zip
+echo $?
+
+print_heading "moving to /app directory"
+cd /app
+echo $?
+
+print_heading "install npm"
+npm install
+echo $?
+
+print_heading "daemon-reload"
+systemctl daemon-reload
+echo $?
+
+print_heading "enable backend"
+systemctl enable backend
+echo $?
+
+print_heading "start backend"
+systemctl start backend
+echo $?
+
+print_heading "install mysql"
+dnf install mysql -y
+echo $?
+
+print_heading "give password"
+mysql -h 172.31.9.13 -uroot -p{mysql_root_password} < /app/schema/backend.sql
+echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,16 @@
+source common.sh
+
+print_heading "install mysql server"
 dnf install mysql-server -y
+echo $?
+
+print_heading "enable mysqld"
 systemctl enable mysqld
+echo $?
+
+print_heading "start mysqld"
 systemctl start mysqld
+echo $?
+
+print_heading "installation of mysqld"
 mysql_secure_installation --set-root-pass ExpenseApp@1
