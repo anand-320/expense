f3cc6d97524e2112fd8530f871df89b31ef3a833
anand <anandanand99599@gmail.com>
2024-03-14 19:56:00 +0530

    updating backend

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,10 @@
 source common.sh
+
 mysql_root_password=$1
+if [ -z "${mysql_root_password}"]; then
+  echo input is missing
+  exit 1
+fi
 
 print_heading "install mysql server"
 dnf install mysql-server -y &>>/tmp/expense.log
@@ -14,5 +19,5 @@ systemctl start mysqld &>>/tmp/expense.log
 print_status $?
 
 print_heading "installation of mysqld"
-mysql_secure_installation --set-root-pass ${mysql_root_password}
+mysql_secure_installation --set-root-pass {mysql_root_password}
 print_status $?
