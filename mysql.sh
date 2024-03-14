1088a3c8192f605df62791b1b3b3ad2346f8aae9
anand <anandanand99599@gmail.com>
2024-03-14 20:10:00 +0530

    updating backend

--- mysql.sh
+++ mysql.sh
@@ -19,7 +19,7 @@ systemctl start mysqld &>>/tmp/expense.log
 print_status $?
 
 print_heading "installation of mysqld"
-echo 'show databases' | mysql -h 172.31.13.176 -uroot -p{mysql_root_password}
+echo 'show databases' | mysql -h 172.31.13.176 -uroot -p{mysql_root_password} &>>/tmp/expense.log
 if [ $? ne 0 ]; then
   mysql_secure_installation --set-root-pass {mysql_root_password} &>>/tmp/expense.log
 fi
